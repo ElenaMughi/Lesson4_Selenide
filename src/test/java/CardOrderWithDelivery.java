d2367f9e8a8e5c3ac31db91ebda9da7e4685598b
ElenaMughi <elena.mughi@gmail.com>
2022-02-12 22:51:11 +0300

    Fix Return N1

--- src/test/java/CardOrderWithDelivery.java
+++ src/test/java/CardOrderWithDelivery.java
@@ -10,16 +10,19 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class CardOrderWithDelivery {
 
-//    WebDriver driver;
-
-    @BeforeAll
-    static void setUpClass() {
-        WebDriverManager.chromedriver().setup();
+    String getDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
+    ;
+
+//    WebDriver driver;
 
     @BeforeEach
     public void setUp() {
@@ -31,30 +34,27 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendSimple() {
-        SetDate date = new SetDate();
+
+        String planningDate = getDate(4);
 
         $("[data-test-id='city'] input").setValue("Москва");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(7));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов Ванечка");
         $("[data-test-id='phone'] input").setValue("+79998887744");
         $("[data-test-id='agreement']").click();
 
         $(withText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldHave(Condition.visible, Duration.ofSeconds(15));
+//        $("[data-test-id='notification']").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
     @Test
     public void shouldSendWithDash() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Ростов-на-Дону");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(30));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(30));
         $("[data-test-id='name'] input").setValue("Иванов-Московский");
         $("[data-test-id='phone'] input").setValue("+09998887744");
         $("[data-test-id='agreement']").click();
@@ -65,13 +65,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendWithSpace() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Марк Аврелий");
         $("[data-test-id='phone'] input").setValue("+49998887744");
         $("[data-test-id='agreement']").click();
@@ -111,51 +108,39 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendWithoutData() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $(withText("Забронировать")).click();
         $("[data-test-id='date'] span").shouldHave(Condition.text("Неверно введена дата"));
     }
 
     @Test
     public void shouldSendTodayData() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(0));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(0));
         $(withText("Забронировать")).click();
         $("[data-test-id='date'] span").shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
     }
 
     @Test
     public void shouldSendTodayPlus2() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(2));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(2));
         $(withText("Забронировать")).click();
         $("[data-test-id='date'] span").shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
     }
 
     @Test
     public void shouldSendFIOEmpty() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("");
         $(withText("Забронировать")).click();
         $("[data-test-id='name'] span").shouldHave(Condition.text("Поле обязательно для заполнения"));
@@ -163,13 +148,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendFIOEng() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Ivanov");
         $(withText("Забронировать")).click();
         $("[data-test-id='name'] span").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -177,13 +159,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendFIONumber() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Николай 2");
         $(withText("Забронировать")).click();
         $("[data-test-id='name'] span").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -191,13 +170,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendPhoneEmpty() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Марк Аврелий");
         $("[data-test-id='phone'] input").setValue("");
         $(withText("Забронировать")).click();
@@ -206,13 +182,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendPhoneNotPlus() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Марк Аврелий");
         $("[data-test-id='phone'] input").setValue("71112223344");
         $(withText("Забронировать")).click();
@@ -221,13 +194,10 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendPhoneManyNumbers() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Марк Аврелий");
         $("[data-test-id='phone'] input").setValue("+711122233448");
         $(withText("Забронировать")).click();
@@ -236,17 +206,17 @@ public class CardOrderWithDelivery {
 
     @Test
     public void shouldSendWithoutAgreement() {
-        SetDate date = new SetDate();
 
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        for (int i = 1; i <= 8; i++) {
-            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        }
-        $("[data-test-id='date'] input").setValue(date.getDate(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Марк Аврелий");
         $("[data-test-id='phone'] input").setValue("+71112223344");
 //        $("[data-test-id='agreement']").click();
         $(withText("Забронировать")).click();
-        $("[data-test-id='agreement'].input_invalid").shouldBe(); //?
+//        $("[data-test-id='agreement'].input_invalid").shouldBe(); //? .input_invalid.input_sub
+//        $("[data-test-id='agreement']").name().contains("input_invalid");
+//        $("[data-test-id='agreement']").data("input_invalid");
+        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']").shouldBe();
     }
 }
